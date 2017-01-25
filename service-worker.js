6d30820b95e6d9d716e1a22b8189987f4a9b8d9a
Andrei <aneagoie1@gmail.com>
2017-01-25 15:07:09 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","0590e0b8d9d2fcc99ee3154a0f38a736"],["manifest.json","1dd07d5a2251f0a789fc7becc35e7139"],["static/css/main.a0d0019b.css","420cf868bdf99953d0707ddc1f7b9363"],["static/js/main.c5d40e94.js","a273bfa8f0bbf21357e53dd24306cb0b"]];
+var precacheConfig = [["index.html","1901e507600f724abcb5bc63cc90ec3c"],["manifest.json","1dd07d5a2251f0a789fc7becc35e7139"],["static/css/main.a0d0019b.css","420cf868bdf99953d0707ddc1f7b9363"],["static/js/main.c5d40e94.js","a273bfa8f0bbf21357e53dd24306cb0b"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
